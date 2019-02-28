a4dc030587f8de7146ab4dac1dad5cf971e2c7b6
huziangWEB <704913915@qq.com>
2019-03-01 04:06:23 +0800

    icon

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,6 +2,7 @@
   <div>
 	<HomeHeader></HomeHeader>
 	<HomeSwiper></HomeSwiper>
+	<HomeIcons></HomeIcons>
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import HomeHeader from '@/components/Header'
 import HomeSwiper from '@/components/Swiper'
+import HomeIcons from '@/components/Icons'
 export default {
 	name: 'Home',
 	components: {
 		HomeHeader,
-		HomeSwiper
+		HomeSwiper,
+		HomeIcons
 	}
 }
 </script>
