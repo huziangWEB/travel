ccad2bfa8ac88337cc210b136a0aa637f0a036d6
huziangWEB <704913915@qq.com>
2019-03-11 16:03:39 +0800

    detail header finsih

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,12 +1,18 @@
 <template>
-	<DetailBanner></DetailBanner>
+	<div>
+		<detail-banner></detail-banner>
+		<detail-header></detail-header>
+		<div style="height: 50rem"></div>
+	</div>	
 </template>
 <script>
 	import DetailBanner from '@/components/detail/Banner'
+	import DetailHeader from '@/components/detail/Header'
 	export default {
 		name: 'Detail',
 		components: {
-			DetailBanner
+			DetailBanner,
+			DetailHeader
 		}
 	}
 </script>
